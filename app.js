f8e8e85a1221aca059870711b011943c620d1326
BrianU <brian.ulatowski@gmail.com>
2020-03-14 22:09:28 -0700

    got all inputs to store and display where they should.

--- app.js
+++ app.js
@@ -1,102 +1,149 @@
 //intern to generate intern file
-const Intern = require("./lib/Intern")
+const Intern = require("./lib/Intern");
 //intern to generate manager file
-const Manager = require("./lib/Manager")
+const Manager = require("./lib/Manager");
 //intern to generate engineer file
-const Engineer= require("./lib/Engineer")
+const Engineer = require("./lib/Engineer");
 //inquirer for prompts
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
 
-const path = require("path")
+const path = require("path");
 //fs for generating html files
-const fs = require("fs")
+const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html")
-​
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
 const render = require("./lib/htmlRenderer");
 
-var employeeList = []
+var employeeList = [];
 
 //prompting the user for name, email, ID, role
+
+function end(list) {
+  console.log(list)
+  // console.log("Inside end");
+
+  return fs.writeFile(outputPath, render(list), function(err) {
+    // console.log("Wrote Html");
+    if (err) {
+      throw err;
+    }
+  });
+}
 function promptUser() {
-    return inquirer.prompt([
-        {
-            type: "list",
-            message: "What is the employee's role?",
-            name: "role",
-            choices: ["Intern", "Engineer", "Manager", "Done. No More Employees."]
-        },
-        {
-            type: "input",
-            message: "What is the employee's name?",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "What is the employee's email?",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "What is the employee's ID?",
-            name: "id"
-        }
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What is the employee's role?",
+        name: "role",
+        choices: ["Intern", "Engineer", "Manager"]
+      },
+      {
+        type: "input",
+        message: "What is the employee's name?",
+        name: "name"
+      },
+      {
+        type: "input",
+        message: "What is the employee's email?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "What is the employee's ID?",
+        name: "id"
+      },
+      {
+        type: "confirm",
+        message: "Need to add anymore employees?",
+        name: "done"
+      }
     ])
 
-.then(response => {
-//continue prompts based on response 
-if (response.role === "Intern") {
-    return inquirer.prompt([
-    {
-        type: "input",
-        message: "What school did you attend?",
-        name: "school"
-    }
-    ])        
-} else if (response.role === "Engineer") {
-    return inquirer.prompt([
-    {
-        type: "input",
-        message: "What is your Github username?",
-        name: "github"
-    }
-    ])    
-} else if (response.role === "Manager") {
-    return inquirer.prompt([
-    {
-        type: "input",
-        message: "Which is the manager's office?",
-        name: "office"
-    } 
-    ])
-} else {
-    //call render function (the employee list)
-    fs.writeFile(outputPath, render(employeeList), function(err){
-        if (err) {
-            throw err
-        }
-    })
-
-.then(resManager => {
-    var newManager = new Manager(response.name, response.id, response.email, resManager.officeNumber)
-    employeeList.push(newManager)
-    promptUser()
-})
-
-.then(resIntern => {
-    var newIntern = new Intern(response.name, response.id, response.email, resIntern.school)
-    employeeList.push(newIntern)
-    promptUser()
-})
-
-.then(resEngineer => {
-    var newEngineer = new Engineer(response.name, response.id, response.email, resEngineer.github)
-    employeeList.push(newEngineer)
-    promptUser()
-})
-
-}}
-)}   
-
-promptUser()
+    .then(response => {
+      const doContinue = response.done === false;
+      //continue prompts based on response
+      if (response.role === "Intern") {
+        // console.log("Made Intern");
+        const newIntern = new Intern(
+          response.name,
+          // response.role,
+          response.id,
+          response.email,
+          response.school
+        );
+        return inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What school did you attend?",
+              name: "school"
+            }
+          ])
+          .then(resIntern => {
+            newIntern["school"] = resIntern.school;
+            employeeList.push(newIntern);
+            if (doContinue) {
+              return end(employeeList)
+            } else{
+              promptUser()
+            }
+          });
+      } else if (response.role === "Engineer") {
+        // console.log("Made an Engineer");
+        const newEngineer = new Engineer(
+          response.name,
+          // response.role,
+          response.id,
+          response.email,
+          response.github
+        );
+        return inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What is your Github username?",
+              name: "github"
+            }
+          ])
+          .then(resEngineer => {
+            newEngineer["github"] = resEngineer.github
+            employeeList.push(newEngineer);
+            if (doContinue) {
+              return end(employeeList)
+            } else {
+              promptUser();
+            }
+          });
+      } else if (response.role === "Manager") {
+        // console.log("Made an Manager");
+        const newManager = new Manager(
+          response.name,
+          // response.role,
+          response.id,
+          response.email,
+          response.officeNumber
+        )
+        return inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "Which is the manager's office?",
+              name: "officeNumber"
+            }
+          ])
+          .then(resManager => {
+            newManager["officeNumber"] = resManager.officeNumber
+            employeeList.push(newManager);
+            if (doContinue) {
+              return end(employeeList)
+            } else {
+              promptUser();
+            }
+          });
+      }
+    });
+}
+promptUser();
